2ce8239017b2a1aeede4e47ec3ec0dd8c03fd62e
Dealilees <105396386+Dealilees@users.noreply.github.com>
2024-05-16 02:52:21 +0600

    Add files via upload

---
+++ products.js
@@ -0,0 +1,60 @@
+const products = [
+    {
+        "id": 1,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "albaspray.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 2,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "beplaincica.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 3,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "tocoboessence.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 4,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "tocoboessence.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 5,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "tocoboessence.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 6,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "jartceramidin 1.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой.Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric."
+    },
+    {
+        "id": 7,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "needlyoil.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой."
+    },
+    {
+        "id": 8,
+        "name":" NEEDLY face light oil",
+        "price": 7000,
+        "image": "needlyoil.png",
+        "description": "Невесомое масло для лица, не оставляющее ощущения жирности и липкости, увлажняет кожу, восстанавливает её естественное сияние, действует против морщин и делает кожу более упругой."
+    }
+
+];
+export default products;
---
+++ script.js
@@ -0,0 +1,31 @@
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
+previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
+signInBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
